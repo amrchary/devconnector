6f6e13c936ab9b8abcb4c50dde0f10e57450b9c5
amrchary <amrchary@gmail.com>
2020-08-15 15:46:01 -0700

    Added support for adding & deleting profile information

--- config/db.js
+++ config/db.js
@@ -4,12 +4,17 @@ const db = config.get('mongoURI');
 
 const connectDB = async () => {
   try {
-    await mongoose.connect(db, {"useNewUrlParser": true});
+    await mongoose.connect(db, {
+      useNewUrlParser: true,
+      useCreateIndex: true,
+      useFindAndModify: false,
+      useUnifiedTopology: true,
+    });
     console.log('MongoDB Connected...');
-  } catch(err) {
+  } catch (err) {
     console.error(err.message);
     process.exit(1);
   }
-}
+};
 
 module.exports = connectDB;
--- models/Profile.js
+++ models/Profile.js
@@ -66,7 +66,7 @@ const ProfileSchema = new mongoose.Schema({
         type: String,
         required: true,
       },
-      fieldOfStudy: {
+      fieldofstudy: {
         type: String,
         required: true,
       },
